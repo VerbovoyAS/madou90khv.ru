07e65e6fd9888bd94707349100e9b3a674aa029f
VerbovoyAS <66475919+VerbovoyAS@users.noreply.github.com>
2025-03-03 22:24:30 +0300

    * fix year_advanced_training

--- src/GutenbergBlock.php
+++ src/GutenbergBlock.php
@@ -794,7 +794,7 @@ final class GutenbergBlock
                                         <?php if($year_advanced_training):?>
                                         <tr>
                                             <th scope="row"  class="p-1" style="width: 35%;">Год повышения квалификации:</th>
-                                            <td class="p-1"><?= (new DateTime($year_advanced_training))->format('Y'); ?></td>
+                                            <td class="p-1"><?= (new DateTime($year_advanced_training))->format('Y') ?: ' '; ?></td>
                                         </tr>
                                         <?php endif;?>
                                         <tr>
